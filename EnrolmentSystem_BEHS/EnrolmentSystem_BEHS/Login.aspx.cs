c0a4f4ff3b9f3c0b440715140e41f7227a10361c
SEANSCS17 <54278072+SEANSCS17@users.noreply.github.com>
2021-10-27 22:04:13 -0600

    Arreglo en login
    Arreglo de las pruebas 1 y 2

--- EnrolmentSystem_BEHS/EnrolmentSystem_BEHS/Login.aspx.cs
+++ EnrolmentSystem_BEHS/EnrolmentSystem_BEHS/Login.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using EnrolmentSystem_BEHS.Logica;
-
+/*Se borraron las pruebas anteriores*/
 namespace EnrolmentSystem_BEHS
 {
     public partial class Login : System.Web.UI.Page
