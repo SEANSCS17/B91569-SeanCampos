1b80361a4feba063cb7b4389b680333803ec3fab
SEANSCS17 <54278072+SEANSCS17@users.noreply.github.com>
2021-10-27 22:06:35 -0600

    Pruebas1Realizadas

--- EnrolmentSystem_BEHS/EnrolmentSystem_BEHS/Login.aspx.cs
+++ EnrolmentSystem_BEHS/EnrolmentSystem_BEHS/Login.aspx.cs
@@ -5,7 +5,20 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using EnrolmentSystem_BEHS.Logica;
+/*Se realizan
 
+
+
+las
+
+
+pruebas 
+
+
+1 
+
+
+aqui*/
 namespace EnrolmentSystem_BEHS
 {
     public partial class Login : System.Web.UI.Page
